3c77464e7f10a13d64400a8d4ec15e7281ff2d7b
GOLDEN SANI <dilababah4@gmail.com>
2024-12-06 11:40:45 +0530

    🍁 │𝙎𝙇 │𝙂𝙊𝙇𝘿𝙀𝙉 𝙈𝙀𝙂𝘼 │𝐌𝐃│𝐕1🍁

--- config.js
+++ config.js
@@ -9,3 +9,6 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubuser content.com/u/106251140?=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "hello iam mega md bot iam alive now",
 };
+
+
+                            🍁 │𝙎𝙇 │𝙂𝙊𝙇𝘿𝙀𝙉 𝙈𝙀𝙂𝘼 │𝐌𝐃│𝐕1🍁
