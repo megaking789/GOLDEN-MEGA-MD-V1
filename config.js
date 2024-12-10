6d16f33d034a8850a87869103a0584317026651f
GOLDEN SANI <dilababah4@gmail.com>
2024-12-10 18:23:48 +0530

    🍁 │𝙎𝙇 │𝙂𝙊𝙇𝘿𝙀𝙉 𝙈𝙀𝙂𝘼 │𝐌𝐃│𝐕1🍁

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubuser content.com/u/106251140?=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "hello iam mega md bot iam alive now",
 };
